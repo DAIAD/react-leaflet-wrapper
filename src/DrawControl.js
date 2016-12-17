b98477d4dcbc80523d32eca986857d2077195d66
Stelios Manousopoulos <smanousopoulos@gmail.com>
2016-12-17 13:54:50 +0200

    redesigned draw control

--- src/DrawControl.js
+++ src/DrawControl.js
@@ -1,9 +1,17 @@
 var React = require('react');
 var L = require('leaflet');
 require('leaflet-draw');
+var GeoJSON = require('./GeoJSON');
 
 var DrawControl = React.createClass({
   
+  getInitialState: function() {
+    return {
+      data: this.props.data,
+      geojson: null,
+    };
+  },
+
   getDefaultProps: function() {
     return {
       position: 'topleft',
@@ -15,49 +23,35 @@ var DrawControl = React.createClass({
   },
 
   componentWillMount: function() {
-
-    this.drawnItems = L.geoJSON();
-    this.props.map.addLayer(this.drawnItems);
-
-    //have to break Control -> Layer logic
-    //since this control needs to know layers on creation
-    this.control = new L.Control.Draw({
-      position: this.props.position,
-      edit: {
-        featureGroup: this.drawnItems,
-        ...this.getEditOptions() 
-      },
-      draw: this.getDrawOptions()
-    });
-    this.control.addTo(this.props.map);
-
-    if (this.props.data) {
-      this.drawnItems.addData(this.props.data);
-      this.drawnItems.setStyle(this.control.options.draw.polygon.shapeOptions);
-    }
-    
     this.props.map.on('draw:created', this.createHandler);
     this.props.map.on('draw:edited', this.editHandler);
     this.props.map.on('draw:deleted', this.deleteHandler);
 
   },
   
+  componentWillUpdate: function(nextProps, nextState) {
+    if (nextState.geojson && !this.state.geojson) {
+      this.control = new L.Control.Draw({
+        position: this.props.position,
+        edit: {
+          featureGroup: nextState.geojson,
+          ...this.getEditOptions() 
+        },
+        draw: this.getDrawOptions()
+      });
+      this.control.addTo(this.props.map);
+    }
+  },
+
   componentWillReceiveProps: function(nextProps) {
-    if (nextProps.data !== this.props.data) {
-      this.drawnItems.clearLayers();
-      if (nextProps.data) {
-        this.drawnItems.addData(nextProps.data);
-        //TODO: need to set style at this point cause it automatically resets to default
-        this.drawnItems.setStyle(this.control.options.draw.polygon.shapeOptions);
-      }
-      
-    }  
+    if (nextProps.data !== this.state.data) {
+      this.setState({ data: nextProps.data });
+    }
   },
 
   componentWillUnmount: function() {
     
     this.control.remove();
-    this.drawnItems.remove();
     
     this.props.map.off('draw:created');
     this.props.map.off('draw:edited');
@@ -70,8 +64,6 @@ var DrawControl = React.createClass({
       polyline: false,
       rectangle: {
         shapeOptions: {
-          color: '#2c3e50',
-          fillColor: '#2980b9',
           ...this.props.style
         }
       },
@@ -81,12 +73,9 @@ var DrawControl = React.createClass({
         allowIntersection: false,
         showArea: true,
         shapeOptions: {
-          color: '#2c3e50',
-          fillColor: '#2980b9',
           ...this.props.style
         }
       },
-      ...this.props.draw
     };
   },
 
@@ -99,56 +88,39 @@ var DrawControl = React.createClass({
   },
 
   createHandler: function (e) {
-    const layer = e.layer;
-
-    /*
-    this.drawnItems.eachLayer(layer => {
-      this.drawnItems.removeLayer(layer);
-      });
-      */
-    this.drawnItems.clearLayers();
-    
-    this.drawnItems.addLayer(layer);
-    
-    if(typeof this.props.onDraw === 'function') {
-      this.props.onDraw.bind(this)(layer);
-    }
-
-    if(typeof this.props.onFeatureChange === 'function') {
-      this.props.onFeatureChange(this.drawnItems.toGeoJSON());
-    }
+    const layer = e.layer.toGeoJSON();
+    this.updateData(layer);
   },
 
   editHandler: function(e) {
-    const layers = e.layers;
-
-    this.drawnItems.clearLayers();
-    
-    layers.eachLayer(layer => {
-      this.drawnItems.addLayer(layer);
-  
-      if(typeof this.props.onDraw === 'function') {
-        this.props.onDraw.bind(this)(layer);
-      }
-    });
-
-    if(typeof this.props.onFeatureChange === 'function') {
-      this.props.onFeatureChange(this.drawnItems.toGeoJSON());
-    }
-
+    const layer = e.layers.toGeoJSON();
+    this.updateData(layer);
   },
 
   deleteHandler: function (e) {
-    const layer = e.layers;
+    this.updateData(null);
+  },
+
+  updateData: function(data) {
+    this.setState({ data });
 
     if(typeof this.props.onFeatureChange === 'function') {
-      this.props.onFeatureChange(this.drawnItems.toGeoJSON());
+      this.props.onFeatureChange(data);
     }
   },
 
   render: function() {
-
-    return null; 
+    return (
+      <GeoJSON
+        ref={geojson => { 
+          if (geojson && !this.state.geojson) { 
+            this.setState({ geojson: geojson.layer}); 
+          }
+        }}
+        {...this.props}
+        data={this.state.data}
+      />
+    );
   }
 });
 
